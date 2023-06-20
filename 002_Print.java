08b720b26aaa845cb6efc308325aa1f45f18fb10
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-06-20 22:12:57 +0700

    Create 002_Print.java

---
+++ 002_Print.java
@@ -0,0 +1,17 @@
+public class UnguidedData {
+    public static void main(String[] args) {
+        String nama = "Aan";
+        int usia = 17;
+        char golDarah = 'A';
+        float tinggiBadan = 170f;
+        double beratBadan = 70;
+        boolean pria = true;
+        
+        System.out.println("Nama            : "+ nama);
+        System.out.println("Usia            : "+ usia);
+        System.out.println("Golongan Darah  : "+ golDarah);
+        System.out.println("Tinggi Badan    : "+ tinggiBadan);
+        System.out.println("Berat Badan     : "+ beratBadan);
+        System.out.println("Pria ?          : "+ pria);
+    }
+}

b00b6fbb843b5823b1798ef044d41acfec60d094
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-07-08 22:33:04 +0700

    Create 017_Exception.java

---
+++ 017_Exception.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sebelum Pembagian");
+        System.out.println("Hasil Bagi : " + 5/0);
+        System.out.println("Setelah Pembagian");
+    }
+}
